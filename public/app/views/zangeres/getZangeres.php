9156c9974dedd5d98c3a3db365b107d4ae7954c1
SemTenz <sem.jaeger@ziggo.nl>
2023-03-14 11:48:51 +0100

    klaar

--- public/app/views/zangeres/getZangeres.php
+++ public/app/views/zangeres/getZangeres.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://mvc-oop-toets.com/public/css/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="http://mvc-oop-toets.com/public/css/toets.css">
+    <title>Mvc-oop-toets</title>
 </head>
 <body>
     <h3><?= $data['title']; ?></h3>
